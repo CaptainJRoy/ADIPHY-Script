2c7b899d36226eaff451bb1713d32bc869a2d52d
CaptainJRoy <CaptainJRoy@users.noreply.github.com>
2017-02-04 18:40:22 +0000

    Update ADIPHY.js

--- ADIPHY.js
+++ ADIPHY.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ADIPHY Script
 // @description  Periodically refreshes Spare5 checking for new tasks for use in background or pinned tabs. Tab icon turns from gray to green when tasks are available.
-// @version      17.01.31
+// @version      17.02.04
 // @author       CaptainRoy
 // @namespace    https://github.com/CaptainJRoy
 // @license      MIT - https://tldrlegal.com/license/mit-license
